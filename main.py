ad80804a6ccedd65a1de60c25fd4625e826a28e8
Sean Robertson <sprobertson@gmail.com>
2025-08-22 11:54:45 -0700

    use base64 for checkmark

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ e = js.React.createElement
 def a(*args): return js.Array(*args)
 def jso(**kwargs): return js.Object.fromEntries(to_js(kwargs))
 
+checkbox_src="data:image/svg+xml;base64,PHN2ZwogICAgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIgogICAgdmlld0JveD0iMCAwIDI0IDI0IgogICAgZmlsbD0ibm9uZSIKICAgIHN0cm9rZT0iI2ZmZiIKICAgIHN0cm9rZS13aWR0aD0iNCIKICAgIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIKICAgIHN0cm9rZS1saW5lam9pbj0icm91bmQiCj4KPHBvbHlsaW5lIHBvaW50cz0iMjAgNiA5IDE3IDQgMTIiPjwvcG9seWxpbmU+Cjwvc3ZnPgoK"
+
 def LabeledCheckbox(props={}, children=[], *args, **kwargs):
     return e('div', {"className": "checkbox-with-label"},
         e('div', {"className": "checkbox-container"},
             e('input', jso(type="checkbox", checked=props["checked"], onClick=props["onClick"], id=props["id"]), None),
-            e('svg', None, e('use', {"href": "icons/check.svg"}, None)),
+            e('img', {"className": "checkmark", "src": checkbox_src}, None),
         ),
         e('label', {"htmlFor": props["id"]}, props["label"]),
     )
