91dd5074dbf68e88ad0f3fadb7fd9f01b1f3a464
Sean Robertson <sprobertson@gmail.com>
2025-08-22 12:55:08 -0700

    consistent use of js object for props

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from pyodide.ffi import create_proxy, to_js
 e = js.React.createElement
 
 def a(*args): return js.Array(*args)
-def jso(**kwargs): return js.Object.fromEntries(to_js(kwargs))
+def o(**kwargs): return js.Object.fromEntries(to_js(kwargs))
 
 checkbox_src="data:image/svg+xml;base64,PHN2ZwogICAgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIgogICAgdmlld0JveD0iMCAwIDI0IDI0IgogICAgZmlsbD0ibm9uZSIKICAgIHN0cm9rZT0iI2ZmZiIKICAgIHN0cm9rZS13aWR0aD0iNCIKICAgIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIKICAgIHN0cm9rZS1saW5lam9pbj0icm91bmQiCj4KPHBvbHlsaW5lIHBvaW50cz0iMjAgNiA5IDE3IDQgMTIiPjwvcG9seWxpbmU+Cjwvc3ZnPgoK"
 
 def LabeledCheckbox(props={}, children=[], *args, **kwargs):
-    return e('div', {"className": "checkbox-with-label"},
-        e('div', {"className": "checkbox-container"},
-            e('input', jso(type="checkbox", checked=props["checked"], onClick=props["onClick"], id=props["id"]), None),
-            e('img', {"className": "checkmark", "src": checkbox_src}, None),
+    return e('div', o(className="checkbox-with-label"),
+        e('div', o(className="checkbox-container"),
+            e('input', o(type="checkbox", checked=props.checked, onClick=props.onClick, id=props.id), None),
+            e('img', o(className="checkmark", src=checkbox_src), None),
         ),
-        e('label', {"htmlFor": props["id"]}, props["label"]),
+        e('label', o(htmlFor=props.id), props.label),
     )
 
 @create_proxy
@@ -49,12 +49,12 @@ def App(props={}, children=[], *args, **kwargs):
         set_use_delay(not use_delay)
 
     return e('div', None,
-        e('p', {"className": "count"}, count),
-        e('div', {"className": "buttons"},
-            e('button', jso(onClick=handle_inc(1)), '+'),
-            e('button', jso(onClick=handle_inc(-1)), '-'),
+        e('p', o(className="count"), count),
+        e('div', o(className="buttons"),
+            e('button', o(onClick=handle_inc(1)), '+'),
+            e('button', o(onClick=handle_inc(-1)), '-'),
         ),
-        LabeledCheckbox({"checked": use_delay, "onClick": toggleDelay, "id": "use_delay", "label": "Use delay"}),
+        LabeledCheckbox(o(checked=use_delay, onClick=toggleDelay, id="use_delay", label="Use delay")),
     )
 
 App
