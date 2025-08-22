34f084ba8e602f9285cef5282ad6935af2896359
Sean Robertson <sprobertson@gmail.com>
2025-08-22 12:10:01 -0700

    it ends with os

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,6 @@ function PythonReader() {
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/ouroborus/',
+    base: '/ouroboros/',
     plugins: [react(), Inspect(), PythonReader()],
 })
